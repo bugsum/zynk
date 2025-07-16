b92445c46b1f9fe455436cb81b85069c6d5cb051
Samarth Sharma <110784602+bugsum@users.noreply.github.com>
2025-07-16 18:55:57 +0530

    Added a verbose ping command

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,14 @@ const EnvFilePath = resolve(process.cwd(), EnvFile);
 
 config({ path: EnvFilePath });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessagePolls,
+        GatewayIntentBits.MessageContent,
+    ],
+});
 
 client.login(process.env.TOKEN);
 
@@ -18,4 +25,19 @@ client.on(Events.ClientReady, (client) => {
 
 client.on(Events.Debug, (message) => {
     console.log(message);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+    const prefix = '?';
+
+    if (
+        !message.guild ||
+        message.author.bot ||
+        !message.content.startsWith(prefix)
+    )
+        return;
+
+    if (message.content.includes('ping')) {
+        message.reply('Here is my ping: ' + client.ws.ping);
+    }
 });
